7f07ef9efbfaf6b7744774efb2d6c9203f080cf8
dom <domalbano35@gmail.com>
2022-07-28 13:05:17 -0400

    randomly generate computer choice

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+console.log('Hello World! Welcome to Rock, Paper, Scissors!!');
+
+// Array to store possible options
+const computerOptions = ['Rock', 'Paper', 'Scissors']
+
 function getComputerChoice() {
-    
+
+    // Picks a random number between 0-2
+   const computerNumber = Math.floor(Math.random() * 3);
+
+    // Sets computer choice as which indecie the random number is
+   const computerChoice = computerOptions[computerNumber]
+
+    console.log(computerChoice)
+};
+
+function playGame(playerSelection, computerSelection) {
+
 }
+
+getComputerChoice(); 
+
+
