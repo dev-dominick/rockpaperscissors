ba8a420ceb601a98ce87dc87aa7a406fca2c7474
dom <domalbano35@gmail.com>
2022-07-29 18:00:30 -0400

    player pick, computer pick, and outcomes display to screen

--- script.js
+++ script.js
@@ -13,34 +13,54 @@ wins.innerHTML = 0;
 losses.innerHTML = 0;
 ties.innerHTML = 0;
 
+// let playerInput = document.getElementById('playerInput');
+let computerInput = document.getElementById('computerInput');
+let winnerStatment = document.getElementById('winnerStatement');
+
+// playerInput.innerHTML = "";
+// computerInput.innerHTML = "";
+// winnerStatement.innerHTML = ""
+
 function getComputerChoice() {
     const computerOptions = ['rock', 'paper', 'scissors']
     // Picks a random number between 0-2
    const computerNumber = Math.floor(Math.random() * 3);
     // Sets computer choice as which indecie the random number is
    const computerSelection = computerOptions[computerNumber]
+
+   document.getElementById('computerInput')
+   
     return computerSelection
 };
 
 function playGame(playerSelection, computerSelection) {
-    // console.log(playerSelection);
+
+    computerInput.innerHTML = computerSelection
+    playerInput.innerHTML = playerSelection
+
+    console.log(playerSelection);
     console.log(computerSelection);
+  
 
         if(playerSelection === computerSelection){
             console.log('Tie');
             gamesPlayed.innerHTML++
             ties.innerHTML++
+            winnerStatment.innerHTML = 'Tie game'
+            console.log('Tie game');
         }
         else if(playerSelection == 'rock'){
 
             if(computerSelection == 'paper'){
                 gamesPlayed.innerHTML++
                 losses.innerHTML++
+                winnerStatment.innerHTML = 'Computer Won, paper beats rock';
                 console.log('Computer Won, paper beats rock');
                 
             }else{
                 gamesPlayed.innerHTML++
                 wins.innerHTML++
+                winnerStatment.innerHTML = 'Player Won, rocks beats scissors';
                 console.log('Player Won, rocks beats scissors');      
             }
         }
@@ -48,10 +68,12 @@ function playGame(playerSelection, computerSelection) {
             if(computerSelection == 'rock'){
                 gamesPlayed.innerHTML++
                 losses.innerHTML++
+                winnerStatment.innerHTML = 'Computer Won, rock beats scissors';
                 console.log('Computer Won, rock beats scissors');  
             }else{
                 gamesPlayed.innerHTML++
                 wins.innerHTML++
+                winnerStatment.innerHTML = 'Player Won, scissors beats paper';
                 console.log('Player Won, scissors beats paper');    
             }
         }
@@ -59,10 +81,12 @@ function playGame(playerSelection, computerSelection) {
             if(computerSelection == 'scissors'){
                 gamesPlayed.innerHTML++
                 losses.innerHTML++
+                winnerStatment.innerHTML = 'Computer Won, scissors beats paper';
                 console.log('Computer Won, scissors beats paper');  
             }else{
                 gamesPlayed.innerHTML++
                 wins.innerHTML++
+                winnerStatment.innerHTML = 'Player Won, paper beats rock';
                 console.log('Player Won, paper beats rock');  
             }
         }
@@ -71,9 +95,8 @@ function playGame(playerSelection, computerSelection) {
 }
 
 function getPlayerChoice() {
-
     let e = document.getElementById("choices");
-
+    
     let playerSelection = e.options[e.selectedIndex].text
 
     console.log(playerSelection);
@@ -84,7 +107,8 @@ function getPlayerChoice() {
 
 function startGame() {
     
-    playGame(getPlayerChoice(), getComputerChoice())
+    playGame(getPlayerChoice(), getComputerChoice());
+
 };
 
 
